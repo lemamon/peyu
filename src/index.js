38139b45bb4696f6a15bbed7449a9b1c81a67656
Deyvid Marinho <dem@certi.org.br>
2018-05-15 16:00:54 -0400

    add bulma css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './assets/css/bulma.min.css'
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
