01471ef5194aaadba95928fa01b3c08670e244e7
Deyvid Marinho <dem@certi.org.br>
2018-05-15 16:01:44 -0400

    add drop down button component

--- src/components/Buttons/index.js
+++ src/components/Buttons/index.js
@@ -13,7 +13,38 @@ export const ButtonLogout = (props) => {
 
   let { text, ...rest } = props;
   return (
-    <button {...rest}>{text}</button>
+    <a {...rest} className="button is-primary">{text}</a>
   )
 }
 
+export const DropDown = (props) => {
+
+  const handleDropDown = () => {
+    let els = document.querySelectorAll('.dropdown');
+    els.forEach(el => {
+      if (el.classList.contains('is-active')) {
+        el.classList.remove('is-active');
+      } else {
+        el.classList.add('is-active');
+      }
+    })
+  }
+
+  return (
+    <div className="dropdown is-right">
+      <div className="dropdown-trigger">
+        <a onClick={handleDropDown} className={props.className} aria-haspopup="true" aria-controls="dropdown-menu3">
+          {props.children}
+        </a>
+      </div>
+      <div className="dropdown-menu" id="dropdown-menu3" role="menu">
+        <div className="dropdown-content">
+          <a className="dropdown-item">Minhas Ocorrências</a>
+          <a className="dropdown-item">Modifiers</a>
+          <hr className="dropdown-divider" />
+          <a onClick={() => props.handleLogout()} className="dropdown-item">Sair</a>
+        </div>
+      </div>
+    </div>
+  )
+}
