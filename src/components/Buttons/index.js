49d47ad27e428450e2cef2000195f85155297ed2
Deyvid Marinho <dem@certi.org.br>
2018-05-15 23:04:27 -0400

    add modal

--- src/components/Buttons/index.js
+++ src/components/Buttons/index.js
@@ -40,11 +40,33 @@ export const DropDown = (props) => {
       <div className="dropdown-menu" id="dropdown-menu3" role="menu">
         <div className="dropdown-content">
           <a className="dropdown-item">Minhas Ocorrências</a>
-          <a className="dropdown-item">Modifiers</a>
+          <a className="dropdown-item">Nova Ocorrência</a>
           <hr className="dropdown-divider" />
           <a onClick={() => props.handleLogout()} className="dropdown-item">Sair</a>
         </div>
       </div>
     </div>
   )
+}
+
+export const Modal = (props) => {
+
+  return (
+    <div className={`modal ${props.show ? 'is-active' : ''}`}>
+      <div className="modal-background"></div>
+      <div className="modal-card">
+        <header className="modal-card-head">
+          <p className="modal-card-title">{props.title}</p>
+          <button onClick={() => props.handleModal(false)} className="delete" aria-label="close"></button>
+        </header>
+        <section className="modal-card-body">
+          {props.children}
+        </section>
+        <footer className="modal-card-foot">
+          <button onClick={() => props.onSubmit()} className="button is-success">Salvar</button>
+          <button onClick={() => props.handleModal(false)} className="button">Cancelar</button>
+        </footer>
+      </div>
+    </div>
+  );
 }
