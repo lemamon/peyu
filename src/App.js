39920ae2bc3fadc3587cadbd998389edbaf6eacd
Deyvid Marinho <dem@certi.org.br>
2018-05-22 11:09:45 -0400

    add LoadingContainer prop to change default Loading

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import MapContainer from './components/Maps/MapContainer'
 import { ButtonLogin, DropDown } from './components/Buttons/'
 import { firebaseAuth, googleProvider } from './constants/config'
 
+import { LoadingContainer } from './components/Loading'
+
 class App extends Component {
 
   state = {
@@ -92,6 +94,7 @@ const logout = () => firebaseAuth().signOut();
 
 export default GoogleApiWrapper({
   apiKey: 'AIzaSyCP0qBpkBupQAlZs9T8wzHEcwYg3k_qW5I',
+  LoadingContainer: LoadingContainer,
 })(App)
 
 
