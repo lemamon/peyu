9ed8431b28cf78ca567ac618e86d7ef5d57f5524
Deyvid Marinho <dem@certi.org.br>
2018-06-08 10:54:45 -0400

    change configs to use firestore

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,15 +1,19 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
+import 'firebase/firestore';
 
 const config = {
-    apiKey: "AIzaSyCO-0K33eZs7z3vahOY5dwpD5TYc-KPXr8",
-    authDomain: "peyu-1526050066204.firebaseapp.com",
-    databaseURL: "https://peyu-1526050066204.firebaseio.com",
+  apiKey: 'AIzaSyCO-0K33eZs7z3vahOY5dwpD5TYc-KPXr8',
+  authDomain: 'peyu-1526050066204.firebaseapp.com',
+  databaseURL: 'https://peyu-1526050066204.firebaseio.com',
+  projectId:'peyu-1526050066204',
 };
 
 firebase.initializeApp(config);
 
 export const googleProvider = new firebase.auth.GoogleAuthProvider();
 export const ref = firebase.database().ref();
+export const db = firebase.firestore();
+export const loc = db.collection('locations');
 export const firebaseAuth = firebase.auth;
